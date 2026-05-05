988c4f127d4cad342226c4b7d3ded53d2ba3b8ed
Garima Pradhananga <garimapradhananga@gmail.com>
2026-05-05 21:11:39 +0545

    fix database creation for production

--- app.py
+++ app.py
@@ -172,8 +172,8 @@ def recommendations():
 
     return render_template("recommendations.html", recommendations=recommended[:12])
 
+iwith app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
- 
